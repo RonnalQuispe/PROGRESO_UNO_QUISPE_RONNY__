07bbac8382837749286b80c1910458142614fdd6
QuispeRonny <ronny.quispe@udla.edu.ec>
2025-04-29 08:03:31 -0500

    agregamos las clases- atributos de la entidad reservas

---
+++ PROGRESO_UNO_QUISPE_RONNY/Models/Reservacs.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROGRESO_UNO_QUISPE_RONNY.Models
+{
+    public class Reservacs
+    {
+        [Key]
+        public int Id_Reserva { get; set }
+        public string Identidicacion { get; set }
+
+        public string Nombre
+    }
+}
