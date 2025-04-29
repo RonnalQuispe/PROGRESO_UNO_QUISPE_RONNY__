0dadc8aa23f5c4aa738d2e25104120bc2a979348
QuispeRonny <ronny.quispe@udla.edu.ec>
2025-04-29 08:11:40 -0500

    agregamos primero la clase reserva porque es dependiente de Ciente

--- PROGRESO_UNO_QUISPE_RONNY/Models/Reservacs.cs
+++ PROGRESO_UNO_QUISPE_RONNY/Models/Reservacs.cs
@@ -4,10 +4,36 @@ namespace PROGRESO_UNO_QUISPE_RONNY.Models
 {
     public class Reservacs
     {
-        [Key]
-        public int Id_Reserva { get; set }
-        public string Identidicacion { get; set }
+        
 
-        public string Nombre
+
+     
+            [Key]
+            public int ReservaId { get; set; }
+
+            [Required]
+            [DataType(DataType.Date)]
+            public DateTime FechaEntrada { get; set; }
+
+            [Required]
+            [DataType(DataType.Date)]
+            public DateTime FechaSalida { get; set; }
+
+            [Required]
+            [Range(0, 10000)]
+            public decimal ValorPagar { get; set; }
+
+            // Relación con cliente
+            public int ClienteId { get; set; }
+            
+        }
     }
+
+
+
+
+
+
+
+}
 }
