adb3606d1dbbcdfe34aaeddbcc928d744a1f3a0b
Usuario <Usuario@DESKTOP-31Q3HS2>
2025-04-29 07:25:57 -0500

    Agregar archivos de proyecto.

---
+++ PROGRESO_UNO_QUISPE_RONNY/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROGRESO_UNO_QUISPE_RONNY.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
